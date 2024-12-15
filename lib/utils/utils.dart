f5f9f46a4d80ce5931decc837d51b9d789f497a8
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-12-15 16:52:02 -0600

    Undo changes

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -5,6 +5,7 @@ import 'dart:io' as io;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:universal_html/html.dart' as html;
 
 enum DeviceType {
@@ -131,6 +132,10 @@ extension ListSwap<T> on List<T> {
 
     return items;
   }
+}
+
+Future<io.Directory?> $applicationDocumentsDirectory() async {
+  return kIsWeb ? null : await getApplicationDocumentsDirectory();
 }
 
 extension BuildContextExtensions on BuildContext {
