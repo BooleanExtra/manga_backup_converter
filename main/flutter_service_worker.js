244cee13af15a9f3aad045e18b3bfac8dcb2a6ac
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-12-17 07:59:14 +0000

    Deploying to gh-pages from @ getBoolean/manga_backup_converter@abc4d946526492e1465b1efebc9af678bd62004e 🚀

--- main/flutter_service_worker.js
+++ main/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "54a3cd9063430781061a50c4006ea200",
-"flutter_bootstrap.js": "9d66360785b97b1b5171da059891eeba"};
+"flutter_bootstrap.js": "f8109c3c7f12872ff0e33c339d5dafca"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
