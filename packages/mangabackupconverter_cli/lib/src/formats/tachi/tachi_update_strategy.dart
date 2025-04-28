7055a7941b233bb4d754d1c56f3b9e8c1696d630
getBoolean <19920697+getBoolean@users.noreply.github.com>
2025-04-27 21:25:41 -0500

    Fix tachi parsing TachiUpdateStrategy

--- packages/mangabackupconverter_cli/lib/src/formats/tachi/tachi_update_strategy.dart
+++ packages/mangabackupconverter_cli/lib/src/formats/tachi/tachi_update_strategy.dart
@@ -2,5 +2,5 @@ import 'package:dart_mappable/dart_mappable.dart';
 
 part 'tachi_update_strategy.mapper.dart';
 
-@MappableEnum(mode: ValuesMode.named, caseStyle: CaseStyle.upperSnakeCase)
+@MappableEnum(mode: ValuesMode.indexed, caseStyle: CaseStyle.upperSnakeCase)
 enum TachiUpdateStrategy { alwaysUpdate, onlyFetchOnce }
