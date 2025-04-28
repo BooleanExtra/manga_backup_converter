2ed8fe54d1355697d9b436756a7343c2dd56a626
getBoolean <19920697+getBoolean@users.noreply.github.com>
2025-04-28 04:18:28 +0000

    Deploying to gh-pages from @ getBoolean/manga_backup_converter@dc4a0ccf39ea488b0d9f5acef02d1a16c58515cf 🚀

--- feature/extensionMapping/flutter_bootstrap.js
+++ feature/extensionMapping/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "360075258"
+    serviceWorkerVersion: "2486596820"
   }
 });
