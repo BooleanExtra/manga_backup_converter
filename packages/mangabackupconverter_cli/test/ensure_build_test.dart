19f05543d08648fba1c07b7f3f81b68a31eb9205
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-02-27 00:51:40 -0600

    Fix ensure build tests

--- packages/mangabackupconverter_cli/test/ensure_build_test.dart
+++ packages/mangabackupconverter_cli/test/ensure_build_test.dart
@@ -1,13 +1,15 @@
 @TestOn('vm')
 @Tags(<String>['presubmit-only'])
 @Timeout(Duration(seconds: 120))
+library;
+
 import 'package:build_verify/build_verify.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/scaffolding.dart';
 
 void main() {
   test(
     'ensure_gen',
-    () => expectBuildClean(packageRelativeDirectory: 'packages/app'),
+    () => expectBuildClean(packageRelativeDirectory: 'packages/mangabackupconverter_cli'),
     // Skipped because build_verify package does not support flutter gen-l10n.
     skip: false,
   );
